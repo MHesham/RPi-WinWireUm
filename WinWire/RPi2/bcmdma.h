320f0e6c77bbd52e13077bcdadbc963e4906a3e3
MHesham <mhesham.fcis@gmail.com>
2015-06-13 14:00:15 -0700

    Add Apache license file and license header to code

--- WinWire/RPi2/bcmdma.h
+++ WinWire/RPi2/bcmdma.h
@@ -1,10 +1,26 @@
-//
-// BCM DMA Controller
-//
+/*
+	Copyright 2015 Muhamad Lotfy
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
 
 #pragma once
 
 #include "RPi2\bcm.h"
+
+//
+// BCM DMA Controller
+//
 
 #define BCM_DMA_BASE_OFFSET			0x00007000
 #define BCM_DMA_CPU_BASE			(BCM_CPU_PERIPH_BASE + BCM_DMA_BASE_OFFSET)
